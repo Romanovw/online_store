ef0edbbd1ef01944ea0458b791606141a6fb8a7f
Volodymyr Romanov <romanov.vw@gmail.com>
2024-03-13 01:17:06 +0100

    categories block

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getCategories } from "../../features/categories/categoriesSlice";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import AppRoutes from "../Routes/Routes";
 import Sidebar from "../Sidebar/Sidebar";
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCategories())
+    }, [dispatch]);
+
     return <div className="app">
         <Header/>
         <div className="container">
